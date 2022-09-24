c0da57472a4b01fd1d4d80592068f3c2429b401f
Adonay Russom <90451352+Adonayrussom@users.noreply.github.com>
2022-09-24 12:33:36 +0300

    Add files via upload

---
+++ HelloClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace sess01_intro
+{
+    /// <summary>
+    /// C# program to display "Hello World" on the console/monitor
+    /// </summary>
+    public class HelloClass
+    {
+        //Main method begins program execution
+        static void Main(string[] args)
+        {
+            //Display a greeting message on the console
+            Console.WriteLine("Hello World from C#");
+        }
+    }
+}
